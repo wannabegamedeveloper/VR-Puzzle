41db5c43676ae234dfdcbced86c7ea9964685bcc
TusharVaid30 <tusharvaid30@gmail.com>
2021-11-12 19:11:59 +0530

    Better Levers

--- Assets/Scripts/Lever/LeverController.cs
+++ Assets/Scripts/Lever/LeverController.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class LeverController : MonoBehaviour
 {
+    [SerializeField] private Transform leverXRot;
+    [SerializeField] private Transform leverZRot;
     [SerializeField] private Transform affectedObject;
     [SerializeField] private float senstivity;
     
     private void Update()
     {
-        Quaternion rotation = Quaternion.Euler(transform.rotation.normalized.x * senstivity, 0f, 0f);
+        Quaternion rotation = Quaternion.Euler(leverXRot.rotation.normalized.x * senstivity, 0f, 
+            leverZRot.rotation.normalized.z * senstivity);
         affectedObject.rotation = rotation;
     }
 }
